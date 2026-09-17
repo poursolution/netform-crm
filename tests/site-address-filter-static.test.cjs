28b38536544d131b564106b5c38cdfe43bb232fa
poursolution <poursolution22@gmail.com>
2026-09-17 17:12:50 +0900

    Surface customer asset address candidates

--- tests/site-address-filter-static.test.cjs
+++ tests/site-address-filter-static.test.cjs
@@ -6,9 +6,11 @@ assert.match(html,/siteAddress:'전체'/);
 assert.match(html,/s\.canonicalAddress=x&&x\.address/);
 assert.match(html,/G\.siteAddress==='완료'&&!!s\.canonicalAddress/);
 assert.match(html,/G\.siteAddress==='미입력'&&!s\.canonicalAddress/);
-assert.match(html,/canonical 주소 '\+addressDone\+' \/ '\+all\.length\+' · 미입력 '\+addressMissing/);
+assert.match(html,/G\.siteAddress==='후보'&&!s\.canonicalAddress&&!!s\.addressSuggestion/);
+assert.match(html,/s\.addressSuggestion=!s\.canonicalAddress&&s\.addresses\.length===1/);
+assert.match(html,/canonical 주소 '\+addressDone\+' \/ '\+all\.length\+' · 확인 후보 '\+addressCandidates\+' · 미입력 '\+addressMissing/);
 assert.match(html,/주소상태<select onchange="G\.siteAddress=this\.value/);
-assert.match(html,/esc\(s\.canonicalAddress\|\|'주소 미입력'\)/);
+assert.match(html,/주소 확인 후보 · /);
 assert.match(html,/blob=\[s\.name,s\.canonicalAddress\|\|''/);
 assert.match(html,/canonical Site 주소 입력 진행률/);
 
